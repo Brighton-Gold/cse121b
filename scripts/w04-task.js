520977cfccefad2600c9c5d592d1faf11d520407
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2023-10-04 20:02:17 -0600

    added favoritefoods, picture, hobbies

---
+++ scripts/w04-task.js
@@ -0,0 +1,65 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+let myProfile = {
+    name:'Brighton M. Gold',
+    photo:"images/a.png",
+    favoriteFoods : [
+        'Sushi', 
+        'Pizza',
+        'Broccolli',
+        'Mushrooms'
+    ], 
+    hobbies : [
+        "Video Games", 
+        "Reading",
+        "Drawing"
+    ],
+    placesLived: [],
+
+};
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: "Rexburg, ID",
+        length:"3 years"
+    }
+);
+myProfile.placesLived.push(
+    {
+        place: "Waynesboro, VA",
+        length:"19 years"
+    }
+);
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+document.getElementById('photo').src = myProfile.photo;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+})
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+})
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let li = document.createElement('li');
+    li.textContent = place;
+    document.querySelector('#places-lived').appendChild(li);
+})
