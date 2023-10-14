b901e99c03abbeea2700e4a7d4072b2a2f6e6d68
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2023-10-13 22:30:03 -0600

    Finshed W5

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,19 +1,19 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templeElements = document.getElementById(temples);
+const templeElements = document.getElementById("temples");
 let templeList = [];
 
 /* async displayTemples Function */
 
-const displayTemples = temples => {
-    templeList.forEach(temple => {
+const displayTemples = (temples) => {
+    temples.forEach((temple) => {
         let articleElement = document.createElement('article');
 
-        let title = document.createElement('title');
+        let title = document.createElement('h3');
         title.textContent = temple.templeName;
 
-        let templePicture = document.createElement('templePicture');
+        let templePicture = document.createElement('img');
         templePicture.src = temple.imageUrl;
         templePicture.alt = temple.location;
 
@@ -45,11 +45,11 @@ const getTemples = async () => {
 /* reset Function */
 const reset = () => {
     const articles = templeElements.querySelectorAll('article');
-    
-    articles.forEach(article => {
+
+    articles.forEach((article) => {
         templeElements.removeChild(article);
-    }
-    )
+      }
+   )
 }
 
 
@@ -57,23 +57,32 @@ const reset = () => {
 function sortBy(temples){
     reset();
 
-    let filter = document.getElementById('#sortBy');
+    let filter = document.getElementById('sortBy').value;
 
     switch(filter) {
         case "utah":
-            displayTemples(temples.location("Utah"));
+            displayTemples(temples.filter(findLocation = (temple) => temple.location.includes("Utah")));
+            break;
 
-        // case "notutah":
+        case "notutah":
+            displayTemples(temples.filter(findLocation = (temple) => temple.location.includes("Utah")== false));
+            break;
 
         case "older":
-            displayTemples(temples.dedicated());
+            displayTemples(temples.filter(getDate = (temple) => parseInt(temple.dedicated.substring(0,4)) < 1950));
+            break;
 
         case "all":
             displayTemples(temples);
-    }
+            break;
+        }
+
+
+
 }
 
 
-
 /* Event Listener */
-DocumentTimeline.querySelector("#sortBy").addEventListener("change", () => { sortBy(templeList)});
+getTemples();
+
+document.querySelector("#sortBy").addEventListener("change", () => { sortBy(templeList) });
